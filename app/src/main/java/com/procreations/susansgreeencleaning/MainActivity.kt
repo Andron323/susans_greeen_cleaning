2f79d84836855b80a82e5ba70dc02b86f39528ec
Andron <opamil323andron@gmail.com>
2023-04-10 15:14:25 +0300

    Initial commit

---
+++ app/src/main/java/com/procreations/susansgreeencleaning/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.procreations.susansgreeencleaning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.procreations.susansgreeencleaning.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        setTheme(R.style.Theme_SusansGreeenCleaning)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        val navHostFragment = activityNavHost as NavHostFragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.activityNavHost) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+}
